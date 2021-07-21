a699127235cf9aaee380b311a0209eddfc727bea
prenassipaolo <prenassipaolo@gmail.com>
2021-07-21 17:55:02 +0200

    change data directory

--- data/download_data.py
+++ data/download_data.py
@@ -4,13 +4,13 @@ from torchvision.transforms import ToTensor
 # download dataset and create dataloader
 def download_cifar_datasets():
     train_data = CIFAR10(
-        root="data",
+        root="./data",
         train=True,
         download=True,
         transform=ToTensor(),
     )
     validation_data = CIFAR10(
-        root="data",
+        root="./data",
         train=False,
         download=True,
         transform=ToTensor(),
