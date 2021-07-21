ba8c1a8073181b50fa6a349e657d524025c8c8ac
massimobini <massimobini.w@gmail.com>
2021-07-21 18:53:10 +0200

    add data preprocessing

--- data/download_data.py
+++ data/download_data.py
@@ -1,18 +1,32 @@
+import torchvision.transforms as transforms
 from torchvision.datasets import CIFAR10
-from torchvision.transforms import ToTensor
 
-# download dataset and create dataloader
+# download dataset
 def download_cifar_datasets():
+    # create transform function
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+    # create datasets
     train_data = CIFAR10(
-        root="data",
+        root="./data",
         train=True,
         download=True,
-        transform=ToTensor(),
+        transform=transform_train,
     )
-    validation_data = CIFAR10(
-        root="data",
+    test_data = CIFAR10(
+        root="./data",
         train=False,
         download=True,
-        transform=ToTensor(),
+        transform=transform_test,
     )
-    return train_data, validation_data
+    return train_data, test_data
