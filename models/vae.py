459d8c79364942587a57e1e324745bce2873b026
massimobini <massimobini.w@gmail.com>
2021-07-26 14:41:23 +0200

    correct feedforward_block  with BatchNorm1D

--- models/vae.py
+++ models/vae.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 
 class Encoder(nn.Module):
-    def __init__(self, in_channels: int, latent_dim : int = 256, hidden_dims: list = [32, 64, 128, 256], im_dim: int = 32, feedforward_block_dim: int = 20):
+    def __init__(self, in_channels: int = 3, latent_dim : int = 256, hidden_dims: list = [32, 64, 128, 256], im_dim: int = 32, feedforward_block_dim: int = 256):
         super().__init__()
 
         modules = []
@@ -25,22 +25,23 @@ class Encoder(nn.Module):
 
         self.encoder = nn.Sequential(*modules)
 
-        encoder_output_H_dim = im_dim / 2 ** len(hidden_dims)
+        encoder_output_H_dim = im_dim // 2 ** len(hidden_dims)
         encoder_output_CxHxW_dim = hidden_dims[-1]*(encoder_output_H_dim ** 2)
 
         self.feedforward_block = nn.Sequential(
                                     nn.Linear(encoder_output_CxHxW_dim, feedforward_block_dim),
-                                    nn.BatchNorm2d(feedforward_block_dim),
+                                    nn.BatchNorm1d(feedforward_block_dim),
                                     nn.LeakyReLU()
                                  )
 
         self.feedforward_mu = nn.Linear(feedforward_block_dim, latent_dim)
         self.feedforward_sigma = nn.Linear(feedforward_block_dim, latent_dim)
 
+
     def forward(self, x):
         x = self.encoder(x)
-        x = self.feedforward_block(x)
         x = torch.flatten(x, start_dim=1)
+        x = self.feedforward_block(x)
 
         mu = self.feedforward_mu(x)
         log_var = self.feedforward_sigma(x)
